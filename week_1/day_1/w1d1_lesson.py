533e87b3256eaa349b3c3f2ebb74577e01da61f8
Yamba Ouandaogo <yamba.ouandaogo@outlook.com>
2023-10-03 10:43:21 -0500

    first commit

---
+++ week_1/day_1/w1d1_lesson.py
@@ -0,0 +1,17 @@
+# Print function
+print('Hello, python is awsome!')
+
+#Strings
+print('He said "What is your name?"')
+print(" my name is Mr Y")
+
+#New line and multi- line 
+print("This is example using \nnewline, which backspace")
+
+#Variables
+# start with a letter or the underscore character
+# cannot start with a number
+# can only contain alpha-numeric characters and underscores
+
+my_variable = "a string"
+print(my_variable)
---
+++ week_1/day_1/w1d1_practice.py
@@ -0,0 +1,21 @@
+# ITP Week 1 Day 1 (In-Class) Practice
+
+# First run this file to see what the expected output is for the following questions.
+
+print('      /|')
+print('     / |')
+print('    /  |')
+print('   /___|')
+
+# recreate this triangle with a height of 6
+
+print('      /|')
+print('     / |')
+print('    /  |')
+print('   /   |')
+print('  /    |')
+print(' /___  |')
+
+# recreate the 4 level triangle with a single line using \n
+print('      /| \n     / | \n    /  |  \n   /___|  ')
+# recreate the 4 level triangle using a multi-line string
