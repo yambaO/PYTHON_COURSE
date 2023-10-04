8cd8e75e76ef3773bb995e48ba39ce0d37b30ec3
Yamba Ouandaogo <yamba.ouandaogo@outlook.com>
2023-10-03 19:45:07 -0500

    Adding lesson day 2

---
+++ week_1/day_2/w1d2_lesson.py
@@ -0,0 +1,28 @@
+#Data type 
+string = "string"
+numeric = 12344
+flt = 12.5
+boolean = True or False
+lst = ["first", "second", 4 , True]
+dict = {"key" : "value"}
+rng = range(6)
+
+# find the data type of the variable
+
+# print(type(string))
+# print(type(numeric))
+# print(type(flt))
+# print(type(boolean))
+# print(type(lst))
+# print(type(dict))
+# print(type(rng))
+
+# Arithmetic Operator 
+
+print("4" + "4")
+print(4 + 4)
+print( 4 - 4)
+print(4 * 4)
+print(4 / 1)
+print(4 % 4)
+print( 4 % 1)
