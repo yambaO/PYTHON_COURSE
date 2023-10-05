a045371788369d00e8a061dcb2f9bd03c2eda778
Yamba Ouandaogo <yamba.ouandaogo@outlook.com>
2023-10-04 19:40:14 -0500

    adding lesson w1d3

---
+++ week_1/day_3/w1d3_lesson.py
@@ -0,0 +1,26 @@
+#BOOLEANS
+
+# print(10 > 9)
+# print(10 == 9)
+# print(10 < 9)
+
+# we can use the bool() to evaluate any value
+
+# print(bool("hello"))
+# print(bool("15.5"))
+# print(bool(""))
+# print(bool(0.0))
+# print(bool(0.1))
+
+# COMPARISON OPERATORS
+
+a = 4  
+b = 5
+c = 5
+
+print( a == b)
+print( b == c)
+print(b != c)
+print( a > b)
+print( b > c)
+print( b >= c)
