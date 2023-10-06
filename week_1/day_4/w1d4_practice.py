11c01ec9fd8377b26364e3247663657647478ba5
Yamba Ouandaogo <yamba.ouandaogo@outlook.com>
2023-10-05 20:32:54 -0500

    adding practice

--- week_1/day_4/w1d4_practice.py
+++ week_1/day_4/w1d4_practice.py
@@ -53,22 +53,32 @@ us_state = ["Alabama",
 ]
 
 # 1. Write the code below that verifies the amount (length) of US States!
+print(len(us_state))
 
 # 2. create a variable my_state_index and assign the index value of the state you currently reside in
+my_state_index = 42
+
 
 # 3. let's see if you were right.. uncomment below and run. Do you see your state?
 
-# print("My state is: " + us_state[my_state_index])
+
+print("My state is: " + us_state[my_state_index])
 
 # 4.Using your state index, let's emphasize some importance to it by *upper*casing it.
     # ASSIGN us_state with my_state_index with us_state of my_state_index with the UPPER method
+us_state[my_state_index] = us_state[my_state_index].upper()
 
 # PRINT us_state to see if there is a change in your state
+# print(us_state)
 
 # 5. POOF. You've been promoted to President! Let's add a new state. I like my list to be alphabetical (which it is)
-# So let's go ahead and create a state that starts with Z and append it to the end of the list.
 
+# So let's go ahead and create a state that starts with Z and append it to the end of the list.
+us_state.append("Zaire")
 
 # 6. There is no state that begins with B! Lets create one and INSERT it appropriately. (There are 4 A states.)
+us_state.insert(4, "Blue")
 
 # 7. Does anyone live in Iowa? Do you know anyone that lives there? Is it even real?! Remove it.. Do it president.
+us_state.remove("Iowa")
+print(us_state)
