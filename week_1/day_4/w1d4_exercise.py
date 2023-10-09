d5d3e48e51d8f09e30c9efbabd2e67b0f55a011f
Yamba Ouandaogo <yamba.ouandaogo@outlook.com>
2023-10-09 12:05:15 -0500

    Adding exercise

--- week_1/day_4/w1d4_exercise.py
+++ week_1/day_4/w1d4_exercise.py
@@ -15,13 +15,14 @@ lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n
 # MEDIUM
 
 # 1. create a new variable called uppercase with an empty list
-uppercase = []
+# uppercase = []
 
 # 2. loop through the lowercase list
     # 2a. append the capitalization of each element to the uppercase list
 
-for single_lowercase in lowercase:
-    print(uppercase.append(single_lowercase))
+# for single_lowercase in lowercase:
+# print(uppercase.append(single_lowercase))
+
 # HARD
 
 # A safe password has a minimum of (1) uppercase, (1) lowercase, (1) number, (1) special character.
@@ -29,6 +30,7 @@ for single_lowercase in lowercase:
 password = "MySuperSafePassword!@34"
 
 special_char = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
+
 
 # 1. create the following variables and assign them Booleans as False
 has_uppercase = False
@@ -38,28 +40,41 @@ has_special_char = False
 
 # 2. loop through the string password (same as a list)
 for character in password:
-   print(password)
 
 # 3. For each iteration of the loop, create a if statement
 # check to see if it exists in any of the list by using IN
 # if it does exist, update the appropriate variable and CONTINUE
 # not break.
 
+ if character.isupper():
+        has_uppercase = True
+ elif character.islower():
+  has_lowercase = True
+ elif character.isdigit():
+  has_number = True
+ elif character in special_char:
+  has_special_char = True
+   
 # NOTE: to see if it has a number, use range from 0 - 10!
 
 # 4. do a final check to see if all of your variables are TRUE
 # by using the AND operator for all 4 conditions. (This is done for you, uncomment below)
 
-# final_result = has_uppercase == True and has_lowercase == True and has_number == True and has_special_char == True
+final_result = has_uppercase == True and has_lowercase == True and has_number == True and has_special_char == True
+print(final_result)
 
 # NOTE: we can shorthand this by just checking if the variable exists (returns True)
-#final_result_shorthand = has_uppercase and has_lowercase and has_number and has_special_char
+final_result_shorthand = has_uppercase and has_lowercase and has_number and has_special_char
+print(final_result_shorthand)
 # this will fail the same if any one of them is False
 
 # If the final_result is true, print "SAFE STRONG PASSWORD"
 # else, print "Update password: too weak"
 # NOTE: this must be done outside of the loop
+if final_result:
+  print("SAFE STRONG PASSWORD")
 
+else: print("Update password: too weak")  
 
 # BONUS: update the password variable to take in an user input!
 
