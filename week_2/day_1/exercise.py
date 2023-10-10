e945f05f42fb49f5d2f7a5acf443c1c2b72551bf
Yamba Ouandaogo <yamba.ouandaogo@outlook.com>
2023-10-10 17:41:43 -0500

    Adding exercise

--- week_2/day_1/exercise.py
+++ week_2/day_1/exercise.py
@@ -23,6 +23,32 @@
 # 	kit_kat	        9276        1000            250                 249
 
 # TIP: create a list of each column (ie. product_names = [oreo, ...]) and use those to loop through :)
+from openpyxl import Workbook
+wb = Workbook()
+ws = wb.active
+ws.title = "CURRENT_MONTH_INVENTORY"
+
+headers = ["product_name", "product_id", "max_amount", "reorder_threshold", "quantity"]
+ws.append(headers) 
+  
+product_data = [
+    ["oreo", 2323, 1000, 300, 743],
+    ["coke", 6545, 500, 100, 101],
+    ["pepsi", 3456, 200, 50, 137],
+    ["lays_chip", 4567, 1500, 500, 364],
+    ["pringles", 2134, 2000, 600, 120],
+    ["sour_worms", 2362, 100, 10, 85],
+    ["choco_cookies", 923, 200, 25, 24],
+    ["donuts", 2786, 200, 25, 12],
+    ["hot_dogs", 6723, 100, 10, 39],
+    ["ice_cream", 9237, 200, 50, 234],
+    ["gum", 2092, 3500, 1000, 1232],
+    ["pretzels", 8246, 100, 5, 11],
+    ["kit_kat", 9276, 1000, 250, 249]
+]
+
+for row in product_data:
+    ws.append(row)
 
 # save your file
-wb.save("./spreadsheets/inventory.xlsx")
+wb.save("/Users/yamba/Python_course/week_2/spreadsheets/inventory.xlsx")
